65b5ea3dc2431fdce65d20a2a37b76eab80ca313
Randall Pruim <rpruim@gmail.com>
2025-08-13 11:40:12 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Shorts Extension For Quarto
+
+Some handy short codes for quarto documents
+
+## Installing
+
+_TODO_: Replace the `<github-organization>` with your GitHub organization.
+
+```bash
+quarto add rpruim/shorts
+```
+
+This will install the extension under the `_extensions` subdirectory.
+If you're using version control, you will want to check in this directory.
+
+## Using
+
+Avalable extensions include
+
+* `{{{ vfill }}} -- add vertical space
+
---
+++ _extensions/shorts/shorts.lua
@@ -0,0 +1,21 @@
+return {
+  ['vfill'] = function(args, kwargs, meta) 
+    local raw = {
+    html = '<br><br><br>',
+    latex = '\\vfill{}',
+    typst = '#v(1fr)',
+  }
+
+  if quarto.doc.isFormat('pdf') then
+    return pandoc.RawBlock('tex', raw.latex)
+  elseif quarto.doc.isFormat('html') then
+    return pandoc.RawBlock('html', raw.html)
+  elseif quarto.doc.isFormat('typst') then
+    return pandoc.RawBlock('typst', raw.typst)
+  else
+    -- fall back 
+    return pandoc.Para{pandoc.Str ''}
+  end
+
+end
+}
