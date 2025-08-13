ac5628e8287e760cae6f3324b196747be8d0f8f7
Randall Pruim <rpruim@gmail.com>
2025-08-13 13:25:27 -0400

    add {{< br >}}

--- _extensions/shorts/shorts.lua
+++ _extensions/shorts/shorts.lua
@@ -1,9 +1,27 @@
 return {
   ['vfill'] = function(args, kwargs, meta) 
     local raw = {
-    html = '<br><br><br>',
+    html = '<br/><br/><br/>',
     latex = '\\vfill{}',
     typst = '#v(1fr)',
+  }
+
+  if quarto.doc.isFormat('pdf') then
+    return pandoc.RawBlock('tex', raw.latex)
+  elseif quarto.doc.isFormat('html') then
+    return pandoc.RawBlock('html', raw.html)
+  elseif quarto.doc.isFormat('typst') then
+    return pandoc.RawBlock('typst', raw.typst)
+  else
+    -- fall back 
+    return pandoc.Para{pandoc.Str ''}
+  end
+
+  ['br'] = function(args, kwargs, meta) 
+    local raw = {
+    html = '<br/><br/>',
+    latex = '\\bigksip{}',
+    typst = '#v(1em)',
   }
 
   if quarto.doc.isFormat('pdf') then
